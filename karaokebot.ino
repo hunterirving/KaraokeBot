7d909c46708b4bec27de17b251ac6e1a8375a8e7
Hunter Irving <8bitvelociraptor@gmail.com>
2018-08-23 23:24:10 -0700

    Add files via upload

---
+++ karaokebot.ino
@@ -0,0 +1,31 @@
+#include "MIDIUSB.h"
+#include "Servo.h"
+
+Servo myservo;
+
+int pos = 0;
+
+void setup() {
+  myservo.attach(9);
+  myservo.write(8);
+}
+
+void loop() {
+  midiEventPacket_t rx;
+  
+  do {
+    rx = MidiUSB.read();
+    if (rx.header != 0) {
+      if (rx.header == 9) /*note on...*/
+      {
+        myservo.write(0); /*...open mouth*/
+      }
+      
+      else if(rx.header == 8) /*note off...*/
+      {
+        myservo.write(8); /*...close mouth*/
+      }
+    }
+  } while (rx.header != 0);
+  
+}
